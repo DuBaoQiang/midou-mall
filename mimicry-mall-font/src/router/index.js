3ff3b5f453ba16a8830806da259766416337053a
DuBaoqiang <du_baoqiang@126.com>
2020-06-30 12:29:40 +0800

    auto import vue-router && vuex component

--- mimicry-mall-font/src/router/index.js
+++ mimicry-mall-font/src/router/index.js
@@ -1,25 +1,30 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue'
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+// auto import router
+const requireRouters = require.context('./', false, /\.router.js$/);
+const routerMap = requireRouters.keys().map(route => requireRouters(route).default)
+
+const routes = [{
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/about',
+        name: 'About',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import ( /* webpackChunkName: "about" */ '../views/About.vue')
+    },
+    ...routerMap
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
 })
 
 export default router
--- mimicry-mall-font/src/store/index.js
+++ mimicry-mall-font/src/store/index.js
@@ -1,12 +1,17 @@
-import Vuex from 'vuex'
+import Vuex from 'vuex';
+
+// vuex auto import
+
+const moudlesFiles = require.context('./modules', false, /\.js$/);
+
+const modules = modulesFiles.keys().reduce((modules, modulePath) => {
+    // set './app.js' => 'app'
+    const moduleName = modulePath.replace(/^\.\/(.*)\.\w+$/, '$1');
+    modules[moduleName] = modulesFiles(modulePath).default;
+    return modules;
+
+}, {})
 
 export default Vuex.createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    modules
 });
