a81151c6f97d6b96bc64191eb7a8223e53dd5c0e
DuBaoqiang <du_baoqiang@126.com>
2020-07-02 14:01:07 +0800

    open note auto router

--- mimicry-mall-font/src/router/index.js
+++ mimicry-mall-font/src/router/index.js
@@ -4,32 +4,32 @@ import Home from '../views/Home/home.vue'
 const requireRouters = require.context('./', false, /\.router.js$/);
 const routerMap = requireRouters.keys().map(route => requireRouters(route).default)
 
-const routes = [
-  {
-    path: '',
-    redirect: '/home',
-    // component: Home
-  },
-  {
-    path: '/home',
-    // redirect: '/mine'
-    component: Home
-  },
-  // ...routerMap,
-  {
-    path: '',
-    component: () => import(/* webpackChunkName: "404" */'../views/404/404.vue')
-  }
+const routes = [{
+        path: '',
+        redirect: '/home',
+        // component: Home
+    },
+    // {
+    //     path: '/home',
+    //     // redirect: '/mine'
+    //     component: Home
+    // },
+    ...routerMap,
+    {
+        path: '',
+        component: () =>
+            import ( /* webpackChunkName: "404" */ '../views/404/404.vue')
+    }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
 })
 
 router.beforeEach((to, from, next) => {
-  if (to.path.endsWith('')) next()
-  else next({ path: to.path + '/', query: to.query, hash: to.hash })
+    if (to.path.endsWith('')) next()
+    else next({ path: to.path + '/', query: to.query, hash: to.hash })
 })
 
 export default router
